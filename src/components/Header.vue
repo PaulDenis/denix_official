87d1f0455a81645880892685f89353ec0a383332
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2021-10-20 16:06:07 +0200

    First setup header complete

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+div {
+    height: 75px;
+    border-bottom: 2px solid cyan;
+}
+
+</style>
