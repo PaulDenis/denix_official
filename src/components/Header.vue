a9ca1ae9ef403ba5cd03b906a924d7e4ecfbb642
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2021-10-21 10:49:09 +0200

    Header now changes the active class dinamically

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,25 @@
   <div class="main-container">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Logo here</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+        <a class="nav-link" 
+        :class="[active_page == 1 ? 'active' : '']"  
+        href="#" 
+        @click="active_page=1">About me</a>
+        <a class="nav-link" 
+        :class="[active_page == 2 ? 'active' : '']" 
+        href="#" 
+        @click="active_page=2">My projects</a>
+        <a class="nav-link" 
+        :class="[active_page == 3 ? 'active' : '']" 
+        href="#" 
+        @click="active_page=3">Contacts</a>
+        <!-- <a class="nav-link disabled">Disabled</a> -->
       </div>
     </div>
   </div>
@@ -21,7 +30,12 @@
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+      return {
+        active_page: 1
+      }
+    },
 }
 </script>
 
