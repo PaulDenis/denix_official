cba786ec3efb04c97f27da0d772c140477ad3c9f
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2021-10-22 13:47:10 +0200

    Cards creation refined. First styiling added.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
       <h1>My Projects</h1>
-      <div v-for="project in projects" :key="project.title">
-        <p>{{ project.title }}</p>
-        <p>{{ project.tecnology }}</p>
-        <p>{{ project.state }}</p>
-        <a v-if="project.state == 'online'" :href='project.url' target=”_blank”>Link to the project</a>
-
+      <div class="project_container">
+        <div v-for="project in projects" :key="project.title" class="project_card">
+            <p><span>Project title:</span> {{ project.title }}</p>
+            <p><span>Tecnologyes used:</span> {{ project.tecnology }}</p>
+            <p><span>Status of the project:</span> {{ project.state }}</p>
+            <a v-if="project.state == 'online'" :href='project.url' target=”_blank”>Link to the project</a>
+            <p v-else>No link avalable</p>
+        </div>
       </div>
   </div>
 </template>
@@ -18,13 +20,13 @@ export default {
         return {
             projects: [
                 {
-                    title: 'Spotify',
+                    title: 'Spotify copy',
                     tecnology: 'HTML, CSS',
                     state: 'online',
                     url: 'https://compassionate-banach-7af1e2.netlify.app/'
                 },
                 {
-                    title: 'Whatsapp Web',
+                    title: 'Boolzapp (A Whatsapp Web copy)',
                     tecnology: 'HTML, CSS, JAVASCRIPT',
                     state: 'online',
                     url: 'https://gallant-kepler-cefe7e.netlify.app/'
@@ -40,7 +42,7 @@ export default {
                     tecnology: 'HTML, CSS, JAVASCRIPT, VUE',
                     state: 'offline',
                     url: ''
-                }
+                },
             ]
         }
     }
@@ -51,5 +53,26 @@ export default {
     .container {
         height: calc(100vh - 75px);
         border-bottom: 2px solid cyan;
+        &>h1 {
+            margin-left: 20px;
+        }
+    }
+    .project_container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+    }
+    .project_card {
+        display: flex;
+        flex-direction: column;
+        width: 30%;
+        margin: 20px;
+        padding: 20px;
+        border-radius: 20px;
+        box-shadow: 0 0 3px black;
+        background-color: rgb(255, 101, 0);
+        & > * {
+            align-self: center;
+        }
     }
 </style>
